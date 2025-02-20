9a12df15dbd29d92ba1aab4b1c53b9f66ec472e9
LaDaniWapa <daniela.diaz@iesnervion.es>
2025-02-20 14:34:24 +0100

    pedidos detalles v0.1

---
+++ src/app/components/detalles-pedido/detalles-pedido.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-detalles-pedido',
+  imports: [],
+  templateUrl: './detalles-pedido.component.html',
+  styleUrl: './detalles-pedido.component.css'
+})
+export class DetallesPedidoComponent {
+
+}
