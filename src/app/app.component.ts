a5f94f7d1fd366647d0eb4a351a7cb298bdbfd56
antoniovizarraga <antonio.vizarraga@iesnervion.es>
2025-02-20 14:34:32 +0100

    Menú de inicio creado Falta terminar el diseño del menú y todo el resto de la vista de inicio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,38 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
+import {NgbCollapseModule} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgIf, NgbCollapseModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'AlimentacionLuna';
+  public isCollapsed = true;
+  private router: Router;
+
+
+  isLogged(): Boolean {
+
+    let respuesta: Boolean = false
+
+    if (this.router.url !== "/login") {
+      respuesta = true;
+    }
+
+    return respuesta;
+  }
+
+  constructor(private _router: Router) {
+    this.router = _router
+
+
+  }
+
+
 }
+
+
