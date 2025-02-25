fba7e8e78c15290aef5d9c0aa04067168a1ff1f7
Eloy Nieves Fernández <eloy.nieves@iesnervion.es>
2025-02-25 10:45:59 +0100

    carritoPedidos test

---
+++ src/app/components/carrito-pedidos/carrito-pedidos.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-carrito-pedidos',
+  imports: [],
+  templateUrl: './carrito-pedidos.component.html',
+  styleUrl: './carrito-pedidos.component.css'
+})
+export class CarritoPedidosComponent {
+
+}
