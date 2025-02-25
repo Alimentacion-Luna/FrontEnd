dac833c0a1590c2f2d571d3f850d05e082fa2731
PabloskyGR <pablogonzalezrufo@gmail.com>
2025-02-25 10:35:19 +0100

    Vista listado pedidos terminada (Cambiar en un futuro para mejorarla)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import {HomeComponent} from './components/home/home.component';
 import {LoginComponent} from './components/login/login.component';
+import { ListadoPedidosComponent } from './components/listado-pedidos/listado-pedidos.component';
 
 export const routes: Routes = [
   {path: '', redirectTo: 'home', pathMatch: 'full'},
   {path: 'home', component: HomeComponent},
-  {path: 'login', component: LoginComponent}
+  {path: 'login', component: LoginComponent},
+  {path: 'listado-pedidos', component: ListadoPedidosComponent},
 ];
