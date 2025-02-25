77f19053bdf962427ab326f8ae0d8c4d5b47172a
Eloy Nieves Fernández <eloy.nieves@iesnervion.es>
2025-02-25 11:58:11 +0100

    CarritoPedidos

--- src/app/components/carrito-pedidos/carrito-pedidos.component.ts
+++ src/app/components/carrito-pedidos/carrito-pedidos.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { FuncsService } from '../../services/funcs.service';
+import { account } from '../../../appwrite';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-carrito-pedidos',
@@ -7,5 +11,23 @@ import { Component } from '@angular/core';
   styleUrl: './carrito-pedidos.component.css'
 })
 export class CarritoPedidosComponent {
+  loggedInUser: any = null;
+    pedidos: any[] = [];
+
+    constructor(private router: Router, private funcs: FuncsService) {
+      // verificar que sea un usuario registrado
+      funcs.getLoggedInUser().then(res => {
+        if (res == null) {
+          this.router.navigate(['/login']);
+        } else {
+          this.loggedInUser = res;
+        }
+      });
+    }
+
+    async logout(): Promise<void> {
+      await account.deleteSession('current');
+      this.router.navigate(['/login']);
+    }
 
 }
