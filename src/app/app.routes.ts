7dbc353fb856cb7cb5d5d05960c7cc33353307ba
Eloy Nieves Fernández <eloy.nieves@iesnervion.es>
2025-02-25 11:56:09 +0100

    Construccion Page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,15 @@ import { Routes } from '@angular/router';
 import {HomeComponent} from './components/home/home.component';
 import {LoginComponent} from './components/login/login.component';
 import { ListadoPedidosComponent } from './components/listado-pedidos/listado-pedidos.component';
+import { CarritoPedidosComponent } from './components/carrito-pedidos/carrito-pedidos.component';
+import { ConstruccionPageComponent } from './components/construccion-page/construccion-page.component';
+
 
 export const routes: Routes = [
   {path: '', redirectTo: 'home', pathMatch: 'full'},
   {path: 'home', component: HomeComponent},
   {path: 'login', component: LoginComponent},
   {path: 'listado-pedidos', component: ListadoPedidosComponent},
+  {path: 'carrito-pedidos', component: CarritoPedidosComponent},
+  {path: 'construccion', component: ConstruccionPageComponent}
 ];
