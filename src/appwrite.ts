f0c583d3c201f0f8d9c2b185eb2131296ab8bff3
LaDaniWapa <urpopolakliente@gmail.com>
2025-03-09 18:16:25 +0100

    cambiar servidor

--- src/appwrite.ts
+++ src/appwrite.ts
@@ -5,8 +5,9 @@ const currentOrigin = window.location.origin; // e.g., "http://localhost:4200"
 export const client = new Client();
 
 client
-  .setEndpoint(`http://localhost:4200/api/v1`)
-  .setProject('67ab896c0025f3acc329');
+  .setProject('67cdcbd00001bdd8b7cd');
+  //.setEndpoint(`http://localhost:4200/api/v1`)
+  //.setProject('67ab896c0025f3acc329');
 
 export const account = new Account(client);
 export {ID} from 'appwrite';
